4712a0282428d5a6403ddcd722cdc6e1f7fe13d2
hlc <1281970998@qq.com>
2021-02-05 20:08:18 +0800

    add function

--- src/elliptic/curves/rsa_group.rs
+++ src/elliptic/curves/rsa_group.rs
@@ -86,12 +86,23 @@ impl Zqf {
         Phi.clone()
     }
 
-    pub fn pow(self, e: &BigInt) -> Self {
+    pub fn pow_mod_m(self, e: &BigInt) -> Self {
         Zqf {
             f: BigInt::mod_pow(&self.f, e, &M),
         }
     }
 
+    pub fn pow_mod_phi(self, e: &BigInt) -> Self {
+        Zqf {
+            f: BigInt::mod_pow(&self.f, e, &Phi),
+        }
+    }
+
+    pub fn sqrt(self) -> Self {
+        Zqf{
+            f: BigInt::sqrt(&self.f).mod_floor(&M),
+        }
+    }
     fn invert(&self) -> Self {
         Zqf {
             f: self.f.invert(&M).unwrap(),
